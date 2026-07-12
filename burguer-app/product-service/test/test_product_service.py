4ac83ad92d48aa669fa7ebb3ee585d35355c8ef6
glpie <gelupie@gmail.com>
2026-07-12 17:11:04 -0300

    First commit

---
+++ burguer-app/product-service/test/test_product_service.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from services import product_service
+
+
+
+if __name__ == "__main__":
+    unittest.main()
